f7a849b58750c921978d554ac498688054dd70d1
Costa Tsaousis <costa@tsaousis.gr>
2017-05-22 10:29:06 +0100

    Mon May 22 09:29:06 UTC 2017 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -1076,7 +1076,7 @@
 [ -f 'proxz_7d.ipset' ] && /usr/bin/touch --date=@1495443632 'proxz_7d.ipset'
 [ -f 'pushing_inertia_blocklist.netset' ] && /usr/bin/touch --date=@1490224106 'pushing_inertia_blocklist.netset'
 [ -f 'ransomware_cryptowall_ps.ipset' ] && /usr/bin/touch --date=@1458596101 'ransomware_cryptowall_ps.ipset'
-[ -f 'ransomware_feed.ipset' ] && /usr/bin/touch --date=@1495444822 'ransomware_feed.ipset'
+[ -f 'ransomware_feed.ipset' ] && /usr/bin/touch --date=@1495445304 'ransomware_feed.ipset'
 [ -f 'ransomware_locky_c2.ipset' ] && /usr/bin/touch --date=@1493639102 'ransomware_locky_c2.ipset'
 [ -f 'ransomware_locky_ps.ipset' ] && /usr/bin/touch --date=@1482450902 'ransomware_locky_ps.ipset'
 [ -f 'ransomware_online.ipset' ] && /usr/bin/touch --date=@1495442426 'ransomware_online.ipset'
